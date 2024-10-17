c40f778491ea2342f8f937d481c03568b7334f64
UtGong <ugong@uw.edu>
2024-10-17 23:57:48 +0800

    feat: Add all model slider controller

--- Assets/Scripts/ModelAnimationController.cs
+++ Assets/Scripts/ModelAnimationController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ModelAnimationController : MonoBehaviour
 {
     public Slider ModelASlider;
-
     public Slider ModelBSlider;
     public Slider ModelCSlider;
+    public Slider AllControlSlider;
 
     public Transform ModelA;
     public Transform ModelB;
     public Transform ModelC;
+
+    public TextMeshProUGUI ModelAIndex;
+    public TextMeshProUGUI ModelBIndex;
+    public TextMeshProUGUI ModelCIndex;
+    public TextMeshProUGUI AllControlIndex;
 
     private List<GameObject> ModelAAnimations = new List<GameObject>();
     private List<GameObject> ModelBAnimations = new List<GameObject>();
@@ -27,21 +33,44 @@ public class ModelAnimationController : MonoBehaviour
         ModelASlider.maxValue = ModelAAnimations.Count - 1;
         ModelBSlider.maxValue = ModelBAnimations.Count - 1;
         ModelCSlider.maxValue = ModelCAnimations.Count - 1;
+
+        InitAllModelSlider();
         
         ModelASlider.onValueChanged.AddListener(delegate
         {
-            ShowAnimationIndex(ModelAAnimations, (int) ModelASlider.value); 
+            ShowAnimationIndex(ModelAAnimations, (int) ModelASlider.value);
+            ModelAIndex.text = ModelASlider.value + "";
         });
         
         ModelBSlider.onValueChanged.AddListener(delegate
         {
             ShowAnimationIndex(ModelBAnimations, (int) ModelBSlider.value); 
+            ModelBIndex.text = ModelBSlider.value + "";
         });
         
         ModelCSlider.onValueChanged.AddListener(delegate
         {
-            ShowAnimationIndex(ModelBAnimations, (int) ModelCSlider.value); 
+            ShowAnimationIndex(ModelCAnimations, (int) ModelCSlider.value);
+            ModelCIndex.text = ModelCSlider.value + "";
         });
+        
+        AllControlSlider.onValueChanged.AddListener(delegate
+        {
+            var _index = (int) AllControlSlider.value;
+            ModelASlider.value = _index;
+            ModelBSlider.value = _index;
+            ModelCSlider.value = _index;
+            AllControlIndex.text = _index + "";
+        });
+    }
+
+    private void InitAllModelSlider()
+    {
+        var max = 0;
+        if (ModelAAnimations.Count > max) max = ModelAAnimations.Count;
+        if (ModelBAnimations.Count > max) max = ModelBAnimations.Count;
+        if (ModelCAnimations.Count > max) max = ModelCAnimations.Count;
+        AllControlSlider.maxValue = max - 1;
     }
 
     private void InitAnimation(Transform parentNode, List<GameObject> anims)
